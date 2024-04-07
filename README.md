a527b1521c5dff0551c9f27810b68aaab7d98f08
Michael Hernando <mhernando334@gmail.com>
2024-04-07 17:40:41 -0400

    testing and decluttering

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # cs-351-hw-04
+This assignment prompted us to create a program that uses frequency analysis to decipher a cipher text given by the user. The program that we created uses frequency analysis to decrypt the cipher text and allows the user to input text that they wish to replace. 
--- app.py
+++ app.py
@@ -1,17 +1,24 @@
 from collections import deque
 
-file = open("text.txt", "r")
-cyphertext = file.read()
-file.close()
+# USED FOR TESTING
+# file = open("text.txt", "r")
+# ciphertext = file.read()
+# file.close()
 
+ciphertext = input("Enter your ciphertext: ")
+
+ciphertext = ciphertext.upper()
+
+# Freq is an ordered list of characters from most frequent to least frequent in the english language
 freq = ['E', 'T', 'A', 'O', 'I', 'N', 'S', 'H', 'R', 'L', 'D', 'C', 'U', 'M', 'W', 'F', 'G', 'Y', 'P', 'B', 'V', 'K', 'J', 'X', 'Q', 'Z']
 
+# Count is a dictionary that represents the frequencies of all letters in the ciphertext
 count = {'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0,
          'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0}
 
-print("The origional cipher text: \n" + cyphertext)
+print("The origional cipher text: \n" + ciphertext)
 
-# Function to fill up dictionary
+# Function to fill up dictionary and sort it 
 def tally(text):
     for char in text:
         char = char
@@ -19,78 +26,56 @@ def tally(text):
             count[char] += 1
     return sorted(count.items(), key=lambda x: x[1], reverse=True)
 
-# Function to clear dictionary
-def clear_all():
-    for char in count.keys():
-        count[char] = 0
-        global ordered 
-        ordered = []
-        
-
 # Array of tuples that represents the ordered count dictionary
-ordered = tally(cyphertext)
+ordered = tally(ciphertext)
 
 # Map the most frequent characters to their assigned character in the frequency list
-oldChars = deque([])
+orgionals = deque([])
 for item in ordered:
-    oldChars.append(item[0])
+    orgionals.append(item[0])
 
-# Sort the dictionary by values in descending order
-
-# print("\nordered", ordered)
-
-# Create a new variable to store the modified plaintext
-decrypted_text = cyphertext
-
-# Replace characters based on frequency analysis
-# def decodeOld():
-#     for index in range(26):
-#         global decrypted_text
-#         decrypted_text = decrypted_text.replace(ordered[index][0],freq[index].upper())
-# decodeOld()
-# print(decrypted_text)
 
 # Replace characters function
 # 1. For every character in the text do the following:
 # 2. Define newText to be the new text
-# 3. If the character is oldChar, add newChar to the new text
-# 4. If the character is newChar, add oldChar to the new text
+# 3. If the character is orgional, add alternate to the new text
+# 4. If the character is alternate, add orgional to the new text
 # 5. Else add the character to the new text
-def repace_characters(oldChar, newChar, text):
+def substitute(orgional, alternate, text):
     newText = ""
     for char in text:
-        if char == oldChar:
-            newText += newChar
-        elif char == newChar:
-            newText += oldChar
+        if char == orgional:
+            newText += alternate
+        elif char == alternate:
+            newText += orgional
         else:
             newText += char
     return newText
 
-newChars = deque(freq)
+alternates = deque(freq)
 
 seen = [] # an array of characters that have already been seen in the de-cipher process
 
 # Decode function
-# 1. While oldChars is not empty or newChars is not empty do the following:
-# 2. Pop from the beginning of the count dictionary and assign it to a variable called oldChar
-# 3. Pop from the beginning of the freq array and assign it to a variable called newChar
+# 1. While orgionals is not empty or alternates is not empty do the following:
+# 2. Pop from the beginning of the count dictionary and assign it to a variable called orgional
+# 3. Pop from the beginning of the freq array and assign it to a variable called alternate
 # 4. If old char or new char is found in the seen array, repeat step 1 and 2
-# 5. Run replace characters with oldChar, newChar, and text
-# 6. oldChar and newChar to seen, and repeat from step 1.
-def decode(cyphertext):
-    text = cyphertext
-    while len(oldChars) > 0 and len(newChars) > 0:
-        oldChar = oldChars.popleft()
-        newChar = newChars.popleft()
-        if oldChar in seen or newChar in seen:
+# 5. Run replace characters with orgional, alternate, and text
+# 6. orgional and alternate to seen, and repeat from step 1.
+def decode(ciphertext):
+    text = ciphertext
+    while len(orgionals) > 0 and len(alternates) > 0:
+        orgional = orgionals.popleft()
+        alternate = alternates.popleft()
+        if orgional in seen or alternate in seen:
             continue
-        text = repace_characters(oldChar, newChar, text)
-        seen.append(oldChar)
-        seen.append(newChar)
+        text = substitute(orgional, alternate, text)
+        seen.append(orgional)
+        seen.append(alternate)
     return text
 
-text = decode(cyphertext)
+text = decode(ciphertext)
 print("\nThe decoded text from frequency analysis: \n" + text)
 
 # User input
@@ -98,12 +83,12 @@ print("\nThe decoded text from frequency analysis: \n" + text)
 user_input = input("\nWould you like to replace letters? (y) yes or (n) no: ")
 if user_input == "y":
     while user_input != "-":
-        oldChar = input("\nEnter the letter that you wish to replace (or submit '-'): ")
-        if oldChar == "-":
+        orgional = input("\nEnter the letter that you wish to replace (or submit '-'): ")
+        if orgional == "-":
             break
-        if oldChar not in text:
-            print(f"'{oldChar}' is not in the text")
+        if orgional not in text:
+            print(f"'{orgional}' is not in the text")
             continue
-        newChar = input("\nEnter the character you would like to replace it with: ")
-        text = repace_characters(oldChar, newChar, text)
+        alternate = input("\nEnter the character you would like to replace it with: ")
+        text = substitute(orgional, alternate, text)
         print("\n" + text)
